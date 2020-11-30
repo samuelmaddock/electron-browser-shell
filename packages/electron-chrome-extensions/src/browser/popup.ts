6364fb67b75dc468797b62ae4bf208dfb5c1a749
samuelmaddock <samuel.maddock@gmail.com>
2020-11-29 22:14:42 -0500

    fix: destroy popup when parent window closes

--- packages/electron-chrome-extensions/src/browser/popup.ts
+++ packages/electron-chrome-extensions/src/browser/popup.ts
@@ -11,10 +11,13 @@ enum PopupGeometry {
 
 export class PopupView {
   browserView?: BrowserView
+  browserWindow?: BrowserWindow
 
   private destroyed: boolean = false
 
-  constructor(public extensionId: string, public browserWindow: BrowserWindow, url: string) {
+  constructor(public extensionId: string, browserWindow: BrowserWindow, url: string) {
+    this.browserWindow = browserWindow
+
     this.browserView = new BrowserView({
       webPreferences: {
         contextIsolation: true,
@@ -41,15 +44,26 @@ export class PopupView {
 
     // TODO:
     this.browserView.webContents.once('blur' as any, this.destroy)
+
+    this.browserWindow.once('closed', this.destroy)
   }
 
   destroy = () => {
     if (this.destroyed) return
 
     if (this.browserView) {
-      this.browserWindow.removeBrowserView(this.browserView)
-      if (this.browserView.webContents.isDevToolsOpened()) {
-        this.browserView.webContents.closeDevTools()
+      if (this.browserWindow) {
+        if (!this.browserWindow.isDestroyed()) {
+          this.browserWindow.off('closed', this.destroy)
+          this.browserWindow.removeBrowserView(this.browserView)
+        }
+        this.browserWindow = undefined
+      }
+
+      const { webContents } = this.browserView
+
+      if (!webContents.isDestroyed() && webContents.isDevToolsOpened()) {
+        webContents.closeDevTools()
       }
 
       this.browserView = undefined
@@ -63,7 +77,7 @@ export class PopupView {
   }
 
   private updatePreferredSize = (event: Electron.Event, size: Electron.Size) => {
-    const windowWidth = this.browserWindow.getSize()[0]
+    const windowWidth = this.browserWindow!.getSize()[0]
     this.browserView?.setBounds({
       x: windowWidth - size.width,
       y: PopupGeometry.OffsetY,
