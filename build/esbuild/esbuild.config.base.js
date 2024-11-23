ecbb5d0627623a3d015a6066e95e9a75f4ce510e
Samuel Maddock <samuel.maddock@gmail.com>
2024-11-23 09:37:53 -0500

    refactor: esbuild base config

--- build/esbuild/esbuild.config.base.js
+++ build/esbuild/esbuild.config.base.js
@@ -1,22 +1,21 @@
 const esbuild = require('esbuild')
 
-function createConfig({ entry, outfile, platform = 'node', external = [], watch = false }) {
+function createConfig(opts = {}) {
   const prod = process.env.NODE_ENV === 'production'
   return {
-    entryPoints: [entry],
-    outfile,
     bundle: true,
-    platform,
+    platform: opts.platform || 'node',
     target: 'es2020',
     sourcemap: !prod,
-    minify: prod,
-    external,
-    watch,
+    minify: false,
+    external: [],
+    watch: false,
     loader: {
       '.ts': 'ts',
       '.tsx': 'tsx',
       '.css': 'css',
     },
+    ...opts,
   }
 }
 
--- packages/electron-chrome-web-store/esbuild.config.js
+++ packages/electron-chrome-web-store/esbuild.config.js
@@ -4,7 +4,7 @@ const { createConfig, build } = require('../../build/esbuild/esbuild.config.base
 console.log(`building ${packageJson.name}`);
 
 const browserConfig = createConfig({
-  entry: 'src/browser/index.ts',
+  entryPoints: ['src/browser/index.ts'],
   outfile: 'dist/browser/index.js',
   platform: 'node',
   external: ['electron'],
@@ -13,7 +13,7 @@ const browserConfig = createConfig({
 build(browserConfig)
 
 const preloadConfig = createConfig({
-  entry: 'src/renderer/web-store-preload.ts',
+  entryPoints: ['src/renderer/web-store-preload.ts'],
   outfile: 'dist/renderer/web-store-preload.js',
   platform: 'browser',
   external: ['electron'],
