a742e3b78d80a6a130524757ba1dbd8b2af1ecb3
Samuel Maddock <samuel.maddock@gmail.com>
2024-12-23 23:10:24 -0500

    fix: workaround net.crash for pending fix https://github.com/electron/electron/pull/45050

--- packages/electron-chrome-web-store/src/browser/utils.ts
+++ packages/electron-chrome-web-store/src/browser/utils.ts
@@ -2,7 +2,16 @@ import * as path from 'node:path'
 import { app, net } from 'electron'
 
 // Include fallbacks for node environments that aren't Electron
-export const fetch = net?.fetch || globalThis.fetch
+export const fetch =
+  // Prefer Node's fetch until net.fetch crash is fixed
+  // https://github.com/electron/electron/pull/45050
+  globalThis.fetch ||
+  net?.fetch ||
+  (() => {
+    throw new Error(
+      'electron-chrome-web-store: Missing fetch API. Please upgrade Electron or Node.',
+    )
+  })
 export const getChromeVersion = () => process.versions.chrome || '131.0.6778.109'
 
 export function compareVersions(version1: string, version2: string) {
