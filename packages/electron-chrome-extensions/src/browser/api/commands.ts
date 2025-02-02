f4eeaa5c0400705934e52535fffedc2694ffaf5a
Samuel Maddock <samuel.maddock@gmail.com>
2025-02-02 14:29:39 -0500

    feat: commands.getAll

--- packages/electron-chrome-extensions/src/browser/api/commands.ts
+++ packages/electron-chrome-extensions/src/browser/api/commands.ts
@@ -1,15 +1,46 @@
 import { ExtensionContext } from '../context'
+import { ExtensionEvent } from '../router'
 
-/**
- * Stub implementation for chrome.commands API.
- */
 export class CommandsAPI {
+  private commandMap = new Map</* extensionId */ string, chrome.commands.Command[]>()
+
   constructor(private ctx: ExtensionContext) {
     const handle = this.ctx.router.apiHandler()
     handle('commands.getAll', this.getAll)
+
+    ctx.session.on('extension-loaded', (_event, extension) => {
+      this.processExtension(extension)
+    })
+
+    ctx.session.on('extension-unloaded', (_event, extension) => {
+      this.removeCommands(extension)
+    })
   }
 
-  getAll() {
-    return []
+  private processExtension(extension: Electron.Extension) {
+    const manifest: chrome.runtime.Manifest = extension.manifest
+    if (!manifest.commands) return
+
+    if (!this.commandMap.has(extension.id)) {
+      this.commandMap.set(extension.id, [])
+    }
+    const commands = this.commandMap.get(extension.id)!
+
+    for (const [name, details] of Object.entries(manifest.commands!)) {
+      // TODO: attempt to register commands
+      commands.push({
+        name,
+        description: details.description,
+        shortcut: '',
+      })
+    }
+  }
+
+  private removeCommands(extension: Electron.Extension) {
+    this.commandMap.delete(extension.id)
+  }
+
+  getAll({ extension }: ExtensionEvent): chrome.commands.Command[] {
+    return this.commandMap.get(extension.id) || []
   }
 }
